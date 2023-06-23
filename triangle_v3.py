390f75fbc8c76a06f4bacced3b95952c97036761
cgathmann <c.gathmann@icloud.com>
2023-06-23 21:52:26 +0200

    lj

--- triangle_v3.py
+++ triangle_v3.py
@@ -18,28 +18,43 @@ lamd = np.linalg.solve(np.array([[1, 1, 1], [0, b[0] - a[0], c[0] - a[0]], [0, b
 
 # create dict for points and edges from mesh
 vertices = dict()
-edges = dict()
 triangles = []
 
 for vectors, normal in zip(stl_mesh.vectors, stl_mesh.normals):
-    edge_list = [abs(vectors[0] - vectors[1]), abs(vectors[1] - vectors[2]), abs(vectors[2] - vectors[0])]
-    for vector, edge in zip(vectors, edge_list):
+    for vector in vectors:
         vector = tuple(vector)
-        edge = tuple(edge)
         normal = tuple(normal)
         if vector not in vertices:
             vertices[vector] = {normal}
         else:
             vertices.get(vector).add(normal)
 
-        if edge not in edges:
-            edges[edge] = {normal}
-        else:
-            edges.get(edge).add(normal)
-
     triangles.append(Triangle(vectors[0], vectors[1], vectors[2], normal))
 
-print(vertices)
-print(edges)
-print(len(edges[tuple(abs(triangles[0].vertex_a - triangles[0].vertex_b))]))
-print(tuple(abs(triangles[0].vertex_a - triangles[0].vertex_b)))
+
+def get_normals_of_vertex(vertex):
+    return list(vertices.get(tuple(vertex)))
+
+
+def get_normals_of_edge(vertex_1, vertex_2):
+    v1 = vertices.get(tuple(vertex_1))
+    v2 = vertices.get(tuple(vertex_2))
+
+    return list(v1.intersection(v2))
+
+
+def test():
+    for vectors in stl_mesh.vectors:
+        print("EDGES:")
+        print(len(get_normals_of_edge(vectors[0], vectors[1])))
+        print(len(get_normals_of_edge(vectors[1], vectors[2])))
+        print(len(get_normals_of_edge(vectors[2], vectors[0])))
+        print()
+        print("VERTICES:")
+        print(len(get_normals_of_vertex(vectors[0])) == 3)
+        print(len(get_normals_of_vertex(vectors[1])) == 3)
+        print(len(get_normals_of_vertex(vectors[2])) == 3)
+        print()
+
+test()
+
